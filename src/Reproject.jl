15a0b1fee91c8e9e625d64ef0e3740f83fc4524c
Ian Weaver <weaveric@gmail.com>
2026-07-23 13:39:02 -0700

    refactor: Use padding built into Interpolations.jl instead of rolling our own

--- src/Reproject.jl
+++ src/Reproject.jl
@@ -4,10 +4,12 @@ using FITSIO: FITS, ImageHDU, read_header
 using Interpolations:
     BSpline,
     Constant,
+    Flat,
     InPlace,
     Linear,
     OnCell,
     Quadratic,
+    extrapolate,
     interpolate
 using SkyCoords: SkyCoords, FK5Coords, GalCoords, ICRSCoords
 using WCS: WCS, WCSTransform, pix_to_world, world_to_pix
